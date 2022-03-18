85170bd6fb35f93067864d01409e4ea8e0338c72
Jefferson Rodrigues <jefferson.rodrigues.mail@gmail.com>
2022-03-17 22:29:10 -0300

    Add hero: Kunkka

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ app.get('/alchemist', function(req, res) {
     );
 });
 
+app.get('/kunkka', function(req, res) {
+    res.json(
+        JSON.parse(fs.readFileSync('src/heroes/kunkka.json'))
+    );
+});
+
 app.listen(3000, () => {
     console.log("Dota app running")
 })
