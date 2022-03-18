1bbb0068001084be5347e5134b17b02957ed29a6
Jefferson Rodrigues <jefferson.rodrigues.mail@gmail.com>
2022-03-17 21:45:47 -0300

    Add hero: Alchemist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 const fs = require('fs');
 
+app.get('/alchemist', function(req, res) {
+    res.json(
+        JSON.parse(fs.readFileSync('src/heroes/alchemist.json'))
+    );
+});
+
 app.listen(3000, () => {
     console.log("Dota app running")
 })
